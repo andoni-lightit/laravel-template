93ceb9a202252fab27979c8f90e2943c68f0fec7
Andoni <andoni.zabala@lightit.io>
2025-12-08 10:20:20 -0300

    feature: creates doctors get endpoint

---
+++ src/Doctors/Domain/Models/Doctor.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Doctors\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Domain\Users\Models\Doctor
+ *
+ * @property int                             $id
+ * @property string                          $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ *
+ * @mixin \Illuminate\Database\Eloquent\Model
+ * @mixin \Eloquent
+ */
+class Doctor extends Model
+{
+}

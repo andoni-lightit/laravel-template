b08a706ef673778d05b7bcaa2f994f41f8dc4bdd
Andoni <andoni.zabala@lightit.io>
2025-12-08 10:33:44 -0300

    fix: wrong class in doctor model

--- src/Doctors/Domain/Models/Doctor.php
+++ src/Doctors/Domain/Models/Doctor.php
@@ -7,20 +7,20 @@ namespace Lightit\Doctors\Domain\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Domain\Users\Models\Doctor
+ * Domain\Doctors\Models\Doctor
  *
  * @property int                             $id
  * @property string                          $name
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  *
- * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Doctor newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Doctor newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Doctor query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Doctor whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Doctor whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Doctor whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Doctor whereUpdatedAt($value)
  *
  * @mixin \Illuminate\Database\Eloquent\Model
  * @mixin \Eloquent

7442eb26d7d6f33c9a9ddc6dde5b5777db07b546
Andoni <andoni.zabala@lightit.io>
2025-12-10 15:56:01 -0300

    feat: Adds Clinic management CRUD endpoints

---
+++ src/Clinics/App/Controllers/UpdateClinicController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Clinics\App\Controllers;
+
+use Dedoc\Scramble\Attributes\Group;
+use Illuminate\Http\JsonResponse;
+use Lightit\Clinics\App\Requests\UpsertClinicRequest;
+use Lightit\Clinics\App\Resources\ClinicResource;
+use Lightit\Clinics\Domain\Actions\UpdateClinicAction;
+use Lightit\Clinics\Domain\Models\Clinic;
+
+
+#[Group('Clinics')]
+final class UpdateClinicController
+{
+    public function __invoke(
+        Clinic $clinic,
+        UpsertClinicRequest $request,
+        UpdateClinicAction $updateClinicAction,
+    ): JsonResponse {
+        $clinic = $updateClinicAction->execute($clinic, $request->toDto());
+
+        return ClinicResource::make($clinic)
+            ->response();
+    }
+}
---
+++ src/Clinics/App/Requests/UpsertClinicRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Clinics\App\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Lightit\Clinics\Domain\DataTransferObjects\ClinicDto;
+
+class UpsertClinicRequest extends FormRequest
+{
+    public const string NAME = 'name';
+    public const string ADDRESS = 'address';
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            self::NAME => ['required', 'string', 'min:4', 'max:80'],
+            self::ADDRESS => ['nullable', 'string', 'min:4', 'max:120'],
+        ];
+    }
+
+    public function toDto(): ClinicDto
+    {
+        return new ClinicDto(
+            name: $this->string(self::NAME)->toString(),
+            address: $this->string(self::ADDRESS)->toString()
+        );
+    }
+}

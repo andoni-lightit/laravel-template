11a4cbd32afb382ab479ad146f851bf21839e2ca
Andoni <andoni.zabala@lightit.io>
2025-12-11 15:15:29 -0300

    feature: add clinic ids on doctor requests

--- src/Doctors/App/Requests/UpsertDoctorRequest.php
+++ src/Doctors/App/Requests/UpsertDoctorRequest.php
@@ -11,6 +11,8 @@ class UpsertDoctorRequest extends FormRequest
 {
     public const string NAME = 'name';
 
+    public const string CLINIC_IDS = 'clinic_ids';
+
     /**
      * @return array<string, mixed>
      */
@@ -18,13 +20,19 @@ class UpsertDoctorRequest extends FormRequest
     {
         return [
             self::NAME => ['required', 'string', 'min:4', 'max:80'],
+            self::CLINIC_IDS => ['array'],
+            self::CLINIC_IDS . '.*' => ['integer', 'exists:clinics,id'],
         ];
     }
 
     public function toDto(): DoctorDto
     {
+        /** @var array<int, int> $clinicIds */
+        $clinicIds = $this->array(self::CLINIC_IDS);
+
         return new DoctorDto(
             name: $this->string(self::NAME)->toString(),
+            clinicIds: $clinicIds
         );
     }
 }
--- src/Doctors/Domain/DataTransferObjects/DoctorDto.php
+++ src/Doctors/Domain/DataTransferObjects/DoctorDto.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Lightit\Doctors\Domain\DataTransferObjects;
 
+
 readonly class DoctorDto
 {
+    /**
+     * @param array<int, int> $clinicIds
+     */
     public function __construct(
         public string $name,
+        public array $clinicIds = [],
     ) {
     }
 }
